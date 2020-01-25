f55e3d2719fcd8bba31bcd0e65dcfc07df322f71
Vinícius Marrara Banaco <vinibanaco@gmail.com>
2020-01-25 20:18:05 -0300

    feat: adiciona base para testes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
 
 function App() {
-  return <h1>Testing Reactotron</h1>;
+  const [users, setUsers] = useState([]);
+
+  /**
+   * Set `users` state with the GitHub API response
+   */
+  useEffect(() => {
+    async function getUsers() {
+      const data = await fetch('https://api.github.com/users')
+        .then((response) => response.json())
+        .then((json) => {
+          return json;
+        });
+
+      setUsers(data);
+    }
+    getUsers();
+  }, []);
+
+  return (
+    <>
+      <h1>Testing Reactotron</h1>
+
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.login}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
